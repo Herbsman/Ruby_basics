c4c518c749897aec7033f4439bf8249624c02c96
Tilen Cizerl <kalfrik@gmail.com>
2015-02-16 03:14:42 +0100

    Cleaned up some more

--- Caesar_Cipher.rb
+++ Caesar_Cipher.rb
@@ -19,28 +19,30 @@
 #If one wishes to move a's to z's, they can just use 25 to do so. (English alphabet has 26letters)
 
 
-def caesar_cipher text, shift = 5 			#shifts letters by 5 on default
+
+
+def caesar_cipher text, shift = 5 										#shifts letters by 5 - by default
 	letters = text.split(//)
 	ncrypted_string = ""
 
 	letters.each do |x|
-		if (x =~ /\w/) && (x =~ /\D/) && (x != "_") 					##Checks with RegEx's whether the current array index' value is a word character + a non-digit character + not an underscore '_', so only a-z & A-Z letters pass the test and are affected by the following code.
-			if x == x.upcase											##<-I do this so I can wrap back to A when Z's #ord index is exceeded. "A".ord == 65, "Z".ord == 90
+		if (x =~ /\w/) && (x =~ /\D/) && (x != "_") ##Checks with RegEx's whether the current array index' value is a word character + a non-digit character + not an underscore '_', so only a-z & A-Z letters pass the test and are affected by the following code.
+			if x == x.upcase						##<-I do this so I can wrap back to A when Z's #ord index is exceeded. "A".ord == 65, "Z".ord == 90
 				x = x.ord + shift
 				if x > 90
 					x -= 90
 					x += 64
 				end
-			elsif x == x.downcase										##Same is done here for downcases as is done above for upcases. "a".ord == 97, "z".ord == 122
+			elsif x == x.downcase					##Same is done here for downcases as is done above for upcases. "a".ord == 97, "z".ord == 122
 				x = x.ord + shift
 				if x > 122
 					x -= 122
 					x += 96
 				end
 			end
-			x.chr 				#convert new index number back to a character
+			x.chr
 		end
-		ncrypted_string << x 	##push current letter into a new string.
+		ncrypted_string << x
 	end
 	puts ncrypted_string
 end
@@ -49,4 +51,4 @@ caesar_cipher("What a string!", 5)					##should modify everything except the end
 
 caesar_cipher("abCDEfg!! __ -- ^^ 77 // ¨¨ ~~", 1)	#Should only modify the leading "abCDEfg"
 
-caesar_cipher("First task complete, 2 to go!")
+caesar_cipher("First task complete, 2 to go!", 26)	#Input should == output
